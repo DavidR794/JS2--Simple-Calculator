054f9cf71cfe95f6875be404caa52d44a49f96b3
DavidR794 <105678612+DavidR794@users.noreply.github.com>
2023-05-01 17:32:00 +0100

    small changes

---
+++ app.js
@@ -0,0 +1,13 @@
+//Intialise......
+/*select the HTML elements that represent the calculator, 
+the screen, all buttons and equal, clear buttons.
+*/
+const screen = document.querySelector(".screen");
+const buttons = document.querySelector(".buttons");
+const equalB = document.querySelector(".btn btn-equal");
+const ClearB = document.querySelector(".btn btn-clear");
+
+
+//Event Listeners
+
+//Execute code
