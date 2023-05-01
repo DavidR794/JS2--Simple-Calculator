aa0eb1f832039348ac87195f3430d7138b55bfdc
DavidR794 <105678612+DavidR794@users.noreply.github.com>
2023-05-01 21:19:40 +0100

    small changes
    completed project

--- app.js
+++ app.js
@@ -1,13 +1,44 @@
+//Steps1> inputs/imports/libraries...
+//Steps2> functions/classes...
+
 //Intialise......
 /*select the HTML elements that represent the calculator, 
 the screen, all buttons and equal, clear buttons.
 */
-const screen = document.querySelector(".screen");
-const buttons = document.querySelector(".buttons");
-const equalB = document.querySelector(".btn btn-equal");
-const ClearB = document.querySelector(".btn btn-clear");
+
+    let screen = document.querySelector('.screen');
+    let buttons = document.querySelectorAll('.btn');
+    let equalB = document.querySelector('.btn-equal'); 
+    let clearB = document.querySelector('.btn-clear');
+  
+//Event Listeners.....
+
+/* All buttons looped with forEach() method
+Event listener waits for button to be clicked
+The clicked buttons data-num attribute is set to the value. e.g. 5=data-num 5
+The value of the button is added to the screen element's value attribute, which displays the value of the button on the calculator screen.
+*/
+    buttons.forEach(function(button){
+      button.addEventListener('click', function(e){
+        let value = e.target.dataset.num;
+        screen.value += value;
+      })
+    });
 
 
-//Event Listeners
+    //equals button clicked , if empty asks please enter, else evaluate whats on the screen and call it result and then update the screen value with the result.
+    equalB.addEventListener('click', function(e){
+        if(screen.value === ''){
+          screen.value = "Please enter";
+        } else {
+          let result = eval(screen.value);
+          screen.value = result;
+        }
+      });
 
-//Execute code
+    clearB.addEventListener('click', function(e){
+    
+            screen.value = "";
+        });
+
+
